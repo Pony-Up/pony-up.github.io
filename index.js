f55fbf38c2fd297023541531b522d0955e1605aa
aceopilot <42447532+pixelfoxagency@users.noreply.github.com>
2024-06-17 12:31:50 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function isMobile() {
+    return navigator.userAgent.toLowerCase().match(/mobile/i);
+}
+
+/* window.addEventListener("load", () => {
+    if (!isMobile()) {
+        console.log("is mobile")
+        var elements = document.getElementsByClassName("worker-app-link");
+        console.log(elements);
+        for (var i = 0; i < elements.length; i++) {
+            console.log(elements[i].href);
+            elements[i].href = "";
+            elements[i].addEventListener("click", function (event) {
+                event.preventDefault()
+                alert("You need a mobile device to download the Pony Up Worker App");
+            });
+        }
+    }
+}) */
